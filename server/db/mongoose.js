2c0e6f857acd8c03ce7f2df841a284256237886b
Ryan Hotton <ryanhotton@gmail.com>
2019-02-27 19:08:38 -0500

    Add PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,5 +4,7 @@ mongoose.Promise = global.Promise;
 
 var mongoDBPath = process.env.MONGO_ATLAS_URI || "mongodb://localhost:27017/TodoApp";
 mongoose.connect(mongoDBPath, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 module.exports = {mongoose};
