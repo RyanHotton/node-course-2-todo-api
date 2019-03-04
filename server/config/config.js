5eff3270b800cb923f2ccac99ce7bfcf339a7910
Ryan Hotton <ryanhotton@gmail.com>
2019-03-04 18:08:14 -0500

    Updated config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.MONGO_ATLAS_URI = "mongodb://localhost:27017/TodoApp";
-    process.env.PORT = 3000;
-} else if (env === 'test') {
-    process.env.MONGO_ATLAS_URI = "mongodb://localhost:27017/TodoAppTest";
-    process.env.PORT = 3000;
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
 
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
